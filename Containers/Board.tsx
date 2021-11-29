419b1a9d162d0975c5a8508eecd1fcaa764c11a9
Alireza Ameri <a.ameri1379@yahoo.com>
2021-11-29 21:17:13 +0330

    add winner calculayion

--- Containers/Board.tsx
+++ Containers/Board.tsx
@@ -1,15 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Square from "../Components/Square";
 import Style from "../styles/Board.module.css";
 
+type player = "X" | "O" | null;
+
 const Board = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [squares, setSquares] = useState<player[]>(Array(9).fill(null));
 
   const [currentPlayer, setCurrentPlayer] = useState<"X" | "O">(
     Math.round(Math.random() * 1) === 1 ? "X" : "O"
   );
 
-  const [winner, setWinner] = useState(null);
+  const [winner, setWinner] = useState<any>(null);
 
   const setSquaresValue = (index: any) => {
     const newData = squares.map((value, i) => {
@@ -30,9 +32,47 @@ const Board = () => {
     setCurrentPlayer(Math.round(Math.random() * 1) === 1 ? "X" : "O");
   };
 
+  const winnerCalculation = (squares: player[]) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  useEffect(() => {
+    const w = winnerCalculation(squares);
+    if (w) {
+      setWinner(w);
+    }
+
+    if (!w && !squares.filter((square) => !square).length) {
+      setWinner("no winner");
+    }
+  });
+
   return (
     <div>
       <p> Hey {currentPlayer} , this is your turn</p>
+
+      {winner && <p>Congratulations {winner} player </p>}
 
       <div className={Style.boardShape}>
         {Array(9)
